70f9b723f54e69799b6f61bc785da1748957f62e
Dawid Jastrzebski <Dawid.Jastrzebski@pb.com>
2020-09-22 14:49:27 +0200

    Hello world project created

---
+++ TextProcessors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextProcessors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
